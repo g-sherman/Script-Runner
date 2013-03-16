63a397e7806364d8813838ec6b3ab81991ab1791
Gary Sherman <gsherman@geoapt.com>
2013-03-15 19:08:15 -0800

    StdoutTextEdit (subclassed from QTextEdit) for capturing stdout by implmenting a write method

---
+++ stdout_textwidget.py
@@ -0,0 +1,50 @@
+"""
+A custom QTextWidget to support writing stdout
+
+ScriptRunner - A QGIS plugin that runs scripts to automate QGIS tasks.
+
+Date: 2013-03-14
+Copyright: (C) 2013 by GeoApt LLC
+Email: gsherman@geoapt.com
+
+
+This program is free software; you can redistribute it and/or modify  
+it under the terms of the GNU General Public License as published by  
+the Free Software Foundation; either version 2 of the License, or     
+(at your option) any later version.                                   
+                                                                          
+"""
+
+#from PyQt4 import QtCore, QtGui
+#from PyQt4.QtCore import Qt, pyqtSignal, QString
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+#from PyQt4.QtGui import QTextCursor
+
+import __init__ as plugin_metatdata
+
+class StdoutTextEdit(QTextEdit):
+    """
+    This class is the stdout widget for Script Runner
+    """
+    new_output = pyqtSignal(str)
+
+    def __init__(self):
+        """
+        Set up the user interface from Designer.
+        """
+        QTextEdit.__init__(self)
+        self.setPlainText("Script Runner %s\n" % plugin_metatdata.version())
+        # cursor for the StdoutTextEdit
+        self.cursor = QTextCursor(self.textCursor())
+        self.setTextCursor(self.cursor)
+
+    def write(self, text):
+        cursor = QTextCursor(self.textCursor())
+        cursor.movePosition(QTextCursor.End)
+        self.setTextCursor(cursor)
+        self.insertPlainText(text)
+        self.ensureCursorVisible()
+        QCoreApplication.processEvents()
+        self.new_output.emit(str(text))
+
